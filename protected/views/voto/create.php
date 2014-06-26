7ce9ed58c87f4adec918262dea69433996492cd4
phantro <phantroc@gmail.com>
2014-06-26 22:51:34 +0200

    Eliminación de código generado

--- protected/views/voto/create.php
+++ protected/views/voto/create.php
@@ -3,18 +3,4 @@
 /* @var $model Voto */
 ?>
 
-<?php
-/*$this->breadcrumbs=array(
-	'Votos'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List Voto', 'url'=>array('index')),
-	array('label'=>'Manage Voto', 'url'=>array('admin')),
-);*/
-?>
-
-<!-- <h1>Create Voto presidencial</h1> -->
-
 <?php $this->renderPartial('_form', array('model'=>$model,'datoCandidato'=>$datoCandidato)); ?>
